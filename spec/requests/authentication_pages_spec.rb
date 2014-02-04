0c4749b6eab1a8eae820d742de550bf583f99106
Frédéric Planté <frederic@asus-laptop>
2014-02-04 14:47:40 +0100

    Finish Exercise 3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,6 +9,11 @@ describe "Authentication" do
 
     it { should have_content('Sign in') }
     it { should have_title('Sign in') }
+    
+    it { should_not have_link('Users') }
+    it { should_not have_link('Profile') }
+    it { should_not have_link('Settings') }
+    it { should_not have_link('Sign out') }    
   end
 
   describe "signin" do
