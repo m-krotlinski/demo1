a8c09a5e41a1da3951adc3097243533a5f346a30
Frédéric Planté <frederic@asus-laptop>
2014-02-04 15:39:16 +0100

    Finish Exercise 6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :redirect_signed_in_user, only: [:new, :create]
   before_action :admin_user,     only: :destroy
 
   def index
@@ -47,6 +48,10 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def redirect_signed_in_user
+      redirect_to root_url if signed_in?
     end
 
 	  def signed_in_user
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -120,4 +120,21 @@ describe "Authentication" do
       specify { expect(response).to redirect_to(root_url) }
     end
   end
+
+  describe "as signed in user" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { sign_in user, no_capybara: true }
+
+    describe "submitting a GET request to the Users#new action" do
+      before { get signup_path }
+      specify { expect(response.body).not_to match(full_title('Sign up')) }
+      specify { expect(response).to redirect_to(root_url) }
+    end
+
+    describe "submitting a POST request to the Users#update action" do
+      before { post users_path }
+      specify { expect(response).to redirect_to(root_url) }
+    end
+  end
+  
 end
